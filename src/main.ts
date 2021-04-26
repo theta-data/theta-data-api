96f4807cd39f53a41ff460da3496bccbe4460453
larryro <371767072@qq.com>
2021-04-26 11:30:00 +0800

    opitimize code

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 const config = require('config')
-console.log('redis', 'redis://' + config.get('REDIS')['host'] + ':' + config.get('REDIS')['port'])
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const microservice = app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.REDIS,
     options: {
-      // port: 5000
       url: 'redis://' + config.get('REDIS')['host'] + ':' + config.get('REDIS')['port']
-      // url: 'redis://' + config.get('REDIS')['host'] + ':' + config.get('REDIS')['port']
     }
   })
   await app.startAllMicroservicesAsync()
