920af1234875f5341b7cf2048053654b87865ae5
larryro <371767072@qq.com>
2021-04-28 10:12:01 +0800

    add msg

--- src/microservice/tx-monitor/micro-tx-monitor.controller.ts
+++ src/microservice/tx-monitor/micro-tx-monitor.controller.ts
@@ -7,12 +7,15 @@ import {
 import { InjectRepository } from '@nestjs/typeorm'
 import { TxMonitorSendListEntity } from '../../tx-monitor/send/tx-monitor-send-list.entity'
 import { LessThan, MoreThan, Repository } from 'typeorm'
+import { TxMonitorWithdrawStakeListEntity } from '../../tx-monitor/withdraw-stake/tx-monitor-withdraw-stake-list.entity'
 const config = require('config')
 @Controller()
 export class MicroTxMonitorController {
   constructor(
     @InjectRepository(TxMonitorSendListEntity)
     private sendTxMonitorRepository: Repository<TxMonitorSendListEntity>,
+    @InjectRepository(TxMonitorWithdrawStakeListEntity)
+    private withdrawTxMonitorRepository: Repository<TxMonitorWithdrawStakeListEntity>,
     @Inject('MAIL_SERVICE') private client: ClientProxy
   ) {}
 
@@ -84,48 +87,63 @@ export class MicroTxMonitorController {
     })
   }
 
-  // @MessagePattern('send-tx-monitor')
-  // async sendTxMonitorTest(
-  //   @Payload()
-  //   {
-  //     token_type,
-  //     amount,
-  //     from,
-  //     to,
-  //     hash
-  //   }: {
-  //     token_type: SendTxMonitorTokenTypeEnum
-  //     amount: number
-  //     from: string
-  //     to: string
-  //     hash: string
-  //   }
-  // ) {
-  //   console.log('get send tx message', token_type, amount, from, to, hash)
-  //   let monitorToSend = await this.sendTxMonitorRepository.find({
-  //     where: {
-  //       token_type: token_type,
-  //       status: SendTxMonitorStatusEnum.success,
-  //       min: MoreThan(amount)
-  //     }
-  //   })
-  //   monitorToSend.forEach((monitor) => {
-  //     this.client.emit<number>('send_mail', {
-  //       from: 'THETA DATA',
-  //       to: monitor.notify_email,
-  //       subject: 'Send Transaction Notification',
-  //       text:
-  //         'from  address ' +
-  //         from +
-  //         ' to ' +
-  //         to +
-  //         ' transfer' +
-  //         amount +
-  //         ' ' +
-  //         token_type +
-  //         ' hash: ' +
-  //         hash
-  //     })
-  //   })
-  // }
+  @EventPattern('withdraw-stake-tx-monitor')
+  async withdrawStakeTxMonitor({
+    from,
+    to,
+    hash
+  }: {
+    // token_type: SendTxMonitorTokenTypeEnum
+    // amount: number
+    from: string
+    to: string
+    hash: string
+  }) {
+    let monitorToSend = await this.withdrawTxMonitorRepository.find({
+      where: {
+        // token_type: token_type,
+        status: SendTxMonitorStatusEnum.success
+        // min: LessThan(amount)
+      }
+    })
+    monitorToSend.forEach((monitor) => {
+      this.client.emit<number>('send_mail', {
+        from: 'THETA DATA',
+        to: monitor.notify_email,
+        subject: 'Notification: new stake withdraw happen.',
+        text: '',
+        // 'from  address ' +
+        // from +
+        // ' to ' +
+        // to +
+        // ' transfer' +
+        // amount +
+        // ' ' +
+        // SendTxMonitorTokenTypeEnum[token_type] +
+        // ' hash: ' +
+        // hash,
+        html:
+          '<p>address:<a href="https://explorer.thetatoken.org/account/' +
+          from +
+          '">' +
+          from +
+          '</a></p><p>withdraw stake ' +
+          // amount +
+          // ' ' +
+          // SendTxMonitorTokenTypeEnum[token_type] +
+          '</p>' +
+          '<p>from: <a href="https://explorer.thetatoken.org/account/">' +
+          to +
+          '</a>' +
+          // to +
+          '</p>' +
+          '<p>transaction hash:</p>' +
+          '<p> <a href="https://explorer.thetatoken.org/txs/' +
+          hash +
+          '"> ' +
+          hash +
+          '</a></p>'
+      })
+    })
+  }
 }
--- src/microservice/tx-monitor/micro-tx-monitor.module.ts
+++ src/microservice/tx-monitor/micro-tx-monitor.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { TxMonitorSendListEntity } from '../../tx-monitor/send/tx-monitor-send-list.entity'
 import { ClientsModule, Transport } from '@nestjs/microservices'
 import { MicroTxMonitorController } from './micro-tx-monitor.controller'
+import { TxMonitorWithdrawStakeListEntity } from '../../tx-monitor/withdraw-stake/tx-monitor-withdraw-stake-list.entity'
 const config = require('config')
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TxMonitorSendListEntity]),
+    TypeOrmModule.forFeature([TxMonitorSendListEntity, TxMonitorWithdrawStakeListEntity]),
     ClientsModule.register([
       {
         name: 'MAIL_SERVICE',
