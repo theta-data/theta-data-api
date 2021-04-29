f984cca2f82851c5a0550b9968ad5fb8e97b3276
larryro <371767072@qq.com>
2021-04-29 09:33:59 +0800

    code opetimize

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt'
 import { PassportStrategy } from '@nestjs/passport'
 import { Injectable } from '@nestjs/common'
 import { jwtConstants } from './constants'
+const config = require('config')
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -9,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret
+      secretOrKey: config.get('jwtConstants').secret
     })
   }
 
