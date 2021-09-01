ec5d1989aa4ca9b10f5f4f6b3a234f863cc94148
larryro <371767072@qq.com>
2021-09-01 21:44:49 +0800

    add node info

---
+++ src/block-chain/stake/stake.model.ts
@@ -0,0 +1,29 @@
+import { Field, Float, Int, ObjectType } from '@nestjs/graphql'
+import { StakeEntity } from './stake.entity'
+
+@ObjectType()
+export class stakeInfo {
+  @Field(() => [StakeEntity])
+  node_info: Array<StakeEntity>
+
+  @Field(() => stakeStatistics)
+  statistics: stakeStatistics
+}
+
+@ObjectType()
+export class stakeStatistics {
+  @Field(() => Int)
+  elite_edge_node_num: number
+
+  @Field(() => Float)
+  theta_stake_ratio: number
+
+  @Field(() => Float)
+  tfuel_stake_ratio: number
+
+  @Field(() => Int)
+  validator_node_num: number
+
+  @Field(() => Int)
+  guardian_node_num: number
+}
--- src/block-chain/stake/stake.resolver.ts
+++ src/block-chain/stake/stake.resolver.ts
@@ -1,13 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Query, ResolveField, Resolver } from '@nestjs/graphql'
 import { StakeService } from './stake.service'
+// import { StakeEntity } from './stake.entity'
+import { stakeInfo } from './stake.model'
 import { StakeEntity } from './stake.entity'
 
-@Resolver()
+@Resolver((of) => stakeInfo)
 export class StakeResolver {
   constructor(private stakeService: StakeService) {}
 
-  @Query(() => [StakeEntity])
-  async stakeEntity() {
-    return this.stakeService.getNodeList()
+  @Query(() => stakeInfo)
+  async stakeInfo() {
+    // let node = await this.stakeService.getNodeList()
+    return {}
+  }
+
+  @ResolveField('node_info', (returns) => [StakeEntity])
+  async getNodeInfo() {
+    return await this.stakeService.getNodeList()
   }
 }
