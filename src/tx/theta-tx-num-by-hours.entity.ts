77e3dc585dc9ac6db40d45f7f822a9bf81f53aec
larryro <371767072@qq.com>
2021-09-06 09:34:18 +0800

    add block number statistics

--- src/tx/theta-tx-num-by-hours.entity.ts
+++ src/tx/theta-tx-num-by-hours.entity.ts
@@ -1,108 +1,120 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn
+} from 'typeorm'
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable'
 
 @Entity()
 @Unique(['year', 'month', 'day', 'hour'])
 export class ThetaTxNumByHoursEntity {
-    @PrimaryGeneratedColumn()
-    id!: number
+  @PrimaryGeneratedColumn()
+  id!: number
 
-    @Column({
-        type : "int"
-    })
-    year : number
+  @Column({
+    type: 'int'
+  })
+  year: number
 
-    @Column({
-        type : "int"
-    })
-    month : number
+  @Column({
+    type: 'int'
+  })
+  month: number
 
-    @Column({
-        type : "int"
-    })
-    day : number
+  @Column({
+    type: 'int'
+  })
+  day: number
 
-    @Column({
-        type : 'int'
-    })
-    hour : number
+  @Column({
+    type: 'int'
+  })
+  hour: number
 
-    @Column({
-        type : "int",
-        default : 0,
-    })
-    coin_base_tx : number
+  @Column({
+    type: 'int'
+  })
+  block_number: number
 
-    @Column({
-        type : "int",
-        default: 0
-    })
-    slash_tx : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  coin_base_tx: number
 
-    @Column({
-        type : "int",
-        default : 0
-    })
-    send_tx : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  slash_tx: number
 
-    @Column({
-        type : "int",
-        default : 0
-    })
-    reserve_fund_tx : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  send_tx: number
 
-    @Column({
-        type : "int",
-        default : 0
-    })
-    release_fund_tx : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  reserve_fund_tx: number
 
-    @Column({
-        type : "int",
-        default : 0
-    })
-    service_payment_tx : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  release_fund_tx: number
 
-    @Column({
-        type : "int",
-        default : 0
-    })
-    split_rule_tx : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  service_payment_tx: number
 
-    @Column({
-        type : "int",
-        default : 0
-    })
-    deposit_stake_tx : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  split_rule_tx: number
 
-    @Column({
-        type : "int",
-        default : 0
-    })
-    withdraw_stake_tx : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  deposit_stake_tx: number
 
-    @Column({
-        type : "int",
-        default : 0
-    })
-    smart_contract_tx:number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  withdraw_stake_tx: number
 
-    @Column({
-        type : "bigint",
-        default : 0
-    })
-    latest_block_height : number
+  @Column({
+    type: 'int',
+    default: 0
+  })
+  smart_contract_tx: number
 
-    @Column({
-        type : 'timestamp',
-        unique :true,
-        comment : "对应精确到小时的数据"
-    })
-    timestamp : string
+  @Column({
+    type: 'bigint',
+    default: 0
+  })
+  latest_block_height: number
 
-    @CreateDateColumn()
-    create_date!: number
+  @Column({
+    type: 'timestamp',
+    unique: true,
+    comment: '对应精确到小时的数据'
+  })
+  timestamp: string
 
-    @UpdateDateColumn()
-    update_date!: number
+  @CreateDateColumn()
+  create_date!: number
 
+  @UpdateDateColumn()
+  update_date!: number
 }
