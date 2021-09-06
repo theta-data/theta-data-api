18c337b97459d467019710c395f61b81bf5fe2e9
larryro <371767072@qq.com>
2021-09-06 10:14:31 +0800

    add data-analyse

--- src/analyse.ts
+++ src/analyse.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { AnalyseService } from './analyse/analyse.service'
+import { AnalyseModule } from './analyse/analyse.module'
 // import { AppService } from './app.service'
 const sleep = require('await-sleep')
 const awaitHors = 2
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule)
+  const app = await NestFactory.createApplicationContext(AnalyseModule)
   const taskService = app.get(AnalyseService)
   // while (1) {
   // await taskService.run()
