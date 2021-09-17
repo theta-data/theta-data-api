5d91b07cc14f802c423e3a2d2c6733e738d54005
larryro <371767072@qq.com>
2021-09-17 17:13:28 +0800

    add theta rpc module gql interface debug

--- src/tx/theta.enum.ts
+++ src/tx/theta.enum.ts
@@ -1,16 +1,18 @@
 import { registerEnumType } from '@nestjs/graphql'
 
 export enum THETA_TX_TYPE_ENUM {
-  coin_base_tx,
-  slash_tx,
-  send_tx,
-  reserve_fund_tx,
-  release_fund_tx,
-  service_payment_tx,
-  split_rule_tx,
-  deposit_stake_tx,
-  withdraw_stake_tx,
-  smart_contract_tx
+  TxCoinbase,
+  TxSlash,
+  TxSend,
+  TxReserveFund,
+  TxReleaseFund,
+  TxServicePayment,
+  TxSplitRule,
+  TxSmartContract,
+  TxDepositStake,
+  TxWithdrawStake,
+  TxDepositStakeV2,
+  TxStakeRewardDistribution
 }
 registerEnumType(THETA_TX_TYPE_ENUM, { name: 'THETA_TX_TYPE_ENUM' })
 
