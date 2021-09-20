335ead04582bb35f38caaa2caa5b7a3ec995b01c
larryro <371767072@qq.com>
2021-09-20 09:46:04 +0800

    debug

--- src/block-chain/rpc/rpc.model.ts
+++ src/block-chain/rpc/rpc.model.ts
@@ -156,10 +156,10 @@ export class transactionRawType {
   @Field({ nullable: true })
   gas_price: string
 
-  @Field(() => [proposerType], { nullable: 'items' })
+  @Field(() => [proposerType], { nullable: 'itemsAndList' })
   from: Array<proposerType>
 
-  @Field(() => [proposerType], { nullable: true })
+  @Field(() => [proposerType], { nullable: 'itemsAndList' })
   to: Array<proposerType>
 
   @Field({ nullable: true })
