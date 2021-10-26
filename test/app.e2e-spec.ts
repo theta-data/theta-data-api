3bd280a8b37a1448c212a56315cab67cd7fbe33e
larryro <371767072@qq.com>
2021-10-26 16:11:02 +0800

    update ci

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -48,88 +48,97 @@ describe('AppController (e2e)', () => {
         return done()
       })
   })
+  it('should get the stake statistics', async (done) => {
+    return request(app.getHttpServer())
+      .post(gql)
+      .send({
+        query:
+          '{\n' +
+          '  StakeStatistics {\n' +
+          '    total_elite_edge_node_number\n' +
+          '    total_guardian_node_number\n' +
+          '    block_height\n' +
+          '    effective_guardian_node_number\n' +
+          '    effective_guardian_stake_amount\n' +
+          '    effective_validator_node_number\n' +
+          '    effective_validator_stake_amount\n' +
+          '    stakes(node_type: validator) {\n' +
+          '      id\n' +
+          '      holder\n' +
+          '      node_type\n' +
+          '      last_signature\n' +
+          '      stakes {\n' +
+          '        amount\n' +
+          '        return_height\n' +
+          '        source\n' +
+          '        withdrawn\n' +
+          '      }\n' +
+          '      update_height\n' +
+          '    }\n' +
+          '    theta_fuel_stake_ratio\n' +
+          '    theta_stake_ratio\n' +
+          '    total_edge_node_stake_amount\n' +
+          '    timestamp\n' +
+          '    total_guardian_stake_amount\n' +
+          '    total_validator_node_number\n' +
+          '    total_validator_stake_amount\n' +
+          '  }\n' +
+          '}\n'
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.StakeStatistics).toHaveProperty('total_elite_edge_node_number')
+        // done()
+      })
+      .end(function (err, res) {
+        if (err) return done(err)
+        return done()
+      })
+  })
+
+  it('should get the theta and theta fuel market information', async (done) => {
+    return request(app.getHttpServer())
+      .post(gql)
+      .send({
+        query:
+          '{' +
+          '  MarketInformation {\n' +
+          '    theta {\n' +
+          '      circulating_supply\n' +
+          '      market_cap\n' +
+          '      last_updated\n' +
+          '      name\n' +
+          '      price\n' +
+          '      total_supply\n' +
+          '      volume_24h\n' +
+          '    }\n' +
+          '    theta_fuel {\n' +
+          '      circulating_supply\n' +
+          '      last_updated\n' +
+          '      name\n' +
+          '      market_cap\n' +
+          '      price\n' +
+          '      total_supply\n' +
+          '      volume_24h\n' +
+          '    }\n' +
+          '  }\n' +
+          '}'
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.MarketInformation).toHaveProperty('theta')
+        expect(res.body.data.MarketInformation).toHaveProperty('theta_fuel')
+        // done()
+      })
+      .end(function (err, res) {
+        if (err) return done(err)
+        return done()
+      })
+  })
+
   afterAll(async () => {
     await app.close()
     // await new Promise((resolve) => setTimeout(() => resolve(0), 500)) // avoid jest open handle error
     // done().catch(() => {})
   })
-  // it('should get the stake statistics', async (done) => {
-  //   return request(app.getHttpServer())
-  //     .post(gql)
-  //     .send({
-  //       query:
-  //         '{\n' +
-  //         '  StakeStatistics {\n' +
-  //         '    total_elite_edge_node_number\n' +
-  //         '    total_guardian_node_number\n' +
-  //         '    block_height\n' +
-  //         '    effective_guardian_node_number\n' +
-  //         '    effective_guardian_stake_amount\n' +
-  //         '    effective_validator_node_number\n' +
-  //         '    effective_validator_stake_amount\n' +
-  //         '    stakes(node_type: validator) {\n' +
-  //         '      id\n' +
-  //         '      holder\n' +
-  //         '      node_type\n' +
-  //         '      last_signature\n' +
-  //         '      stakes {\n' +
-  //         '        amount\n' +
-  //         '        return_height\n' +
-  //         '        source\n' +
-  //         '        withdrawn\n' +
-  //         '      }\n' +
-  //         '      update_height\n' +
-  //         '    }\n' +
-  //         '    theta_fuel_stake_ratio\n' +
-  //         '    theta_stake_ratio\n' +
-  //         '    total_edge_node_stake_amount\n' +
-  //         '    timestamp\n' +
-  //         '    total_guardian_stake_amount\n' +
-  //         '    total_validator_node_number\n' +
-  //         '    total_validator_stake_amount\n' +
-  //         '  }\n' +
-  //         '}\n'
-  //     })
-  //     .expect(200)
-  //     .expect((res) => {
-  //       expect(res.body.data.StakeStatistics).toHaveProperty('total_elite_edge_node_number')
-  //       done()
-  //     })
-  // })
-  //
-  // it('should get the theta and theta fuel market information', async (done) => {
-  //   return request(app.getHttpServer())
-  //     .post(gql)
-  //     .send({
-  //       query:
-  //         '{' +
-  //         '  MarketInformation {\n' +
-  //         '    theta {\n' +
-  //         '      circulating_supply\n' +
-  //         '      market_cap\n' +
-  //         '      last_updated\n' +
-  //         '      name\n' +
-  //         '      price\n' +
-  //         '      total_supply\n' +
-  //         '      volume_24h\n' +
-  //         '    }\n' +
-  //         '    theta_fuel {\n' +
-  //         '      circulating_supply\n' +
-  //         '      last_updated\n' +
-  //         '      name\n' +
-  //         '      market_cap\n' +
-  //         '      price\n' +
-  //         '      total_supply\n' +
-  //         '      volume_24h\n' +
-  //         '    }\n' +
-  //         '  }\n' +
-  //         '}'
-  //     })
-  //     .expect(200)
-  //     .expect((res) => {
-  //       expect(res.body.data.MarketInformation).toHaveProperty('theta')
-  //       expect(res.body.data.MarketInformation).toHaveProperty('theta_fuel')
-  //       done()
-  //     })
-  // })
 })
