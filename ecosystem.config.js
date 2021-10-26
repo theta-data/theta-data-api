fbfbb299a0d4bd99f7aef6c523fe4f5e6c8d3a3c
larryro <371767072@qq.com>
2021-10-26 11:56:40 +0800

    add ci

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       watch: false,
       max_memory_restart: '2G',
       env_test: {
-        NODE_ENV: 'test',
+        NODE_ENV: 'production',
         NODE_CONFIG_DIR: '/home/ubuntu/actions-runner/_work/config/config/theta-data-api'
         // TZ: 'Asia/Shanghai'
       },
