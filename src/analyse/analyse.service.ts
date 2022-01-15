a4816d2bf094d0bf44ca3fcef7508c67f17bb1a0
larryro <371767072@qq.com>
2022-01-15 09:55:11 +0800

    test

--- src/analyse/analyse.service.ts
+++ src/analyse/analyse.service.ts
@@ -39,7 +39,6 @@ export class AnalyseService {
   public async queryDataFromBlockChain() {
     let height =
       Number((await thetaTsSdk.blockchain.getStatus()).result.latest_finalized_block_height) - 1000
-    // height = 10000000
     height = 13209601
     const latestBlock = await this.thetaTxNumByHoursRepository.findOne({
       order: {
