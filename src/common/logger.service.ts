f8dbfdd9b875cf81101921a9067371af3ac4b528
larryro <371767072@qq.com>
2022-03-03 17:36:14 +0800

    log otimization

--- src/common/logger.service.ts
+++ src/common/logger.service.ts
@@ -1,8 +1,14 @@
-// import { Injectable, Scope, Logger } from '@nestjs/common'
-
-// @Injectable({ scope: Scope.TRANSIENT })
-// export class Logger extends Logger {
-//   customLog() {
-//     // this.log('Please feed the cat!')
-//   }
-// }
+import { Injectable, Scope, ConsoleLogger } from '@nestjs/common'
+const moment = require('moment')
+@Injectable({ scope: Scope.TRANSIENT })
+export class LoggerService extends ConsoleLogger {
+  minTs = 3
+  //   logger = new Logger()
+  timeMonitor(name: string, startTime: number) {
+    const usedTime = moment().unix() - startTime
+    if (usedTime > this.minTs) {
+      this.log(name + ' used:' + usedTime)
+    }
+    // this.log('Please feed the cat!')
+  }
+}
