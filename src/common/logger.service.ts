2b71a1c5a50fceed5ae502718708533728303e77
larryro <371767072@qq.com>
2022-03-03 11:26:23 +0800

    debug

---
+++ src/common/logger.service.ts
@@ -0,0 +1,8 @@
+// import { Injectable, Scope, Logger } from '@nestjs/common'
+
+// @Injectable({ scope: Scope.TRANSIENT })
+// export class Logger extends Logger {
+//   customLog() {
+//     // this.log('Please feed the cat!')
+//   }
+// }
