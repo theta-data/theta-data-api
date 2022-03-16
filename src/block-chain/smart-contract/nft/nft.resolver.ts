450a2a7cc0d0c4f3b66c7589a9a53ee325958131
larryro <371767072@qq.com>
2022-03-16 17:17:38 +0800

    code optimization

--- src/block-chain/smart-contract/nft/nft.resolver.ts
+++ src/block-chain/smart-contract/nft/nft.resolver.ts
@@ -1,8 +1,9 @@
 import { Args, Int, Query, ResolveField, Resolver } from '@nestjs/graphql'
+import { buffer } from 'stream/consumers'
 import { SmartContractEntity } from '../smart-contract.entity'
 import { NftBalanceEntity } from './nft-balance.entity'
 import { NftTransferRecordEntity } from './nft-transfer-record.entity'
-import { NftMetaType, NftType } from './nft.model'
+import { NftMetaType, NftType, PaginatedNftBalance } from './nft.model'
 import { NftService } from './nft.service'
 
 @Resolver(() => NftType)
@@ -19,9 +20,29 @@ export class NftResolver {
     return await this.nftService.findNftsByName(name)
   }
 
-  @ResolveField(() => [NftBalanceEntity])
-  async Balance(@Args('wallet_address') walletAddress: string) {
-    return await this.nftService.getNftByWalletAddress(walletAddress.toLowerCase())
+  @ResolveField(() => PaginatedNftBalance)
+  async Balance(
+    @Args('wallet_address') walletAddress: string,
+    @Args('take', { type: () => Int, defaultValue: 10 }) take: number,
+    @Args('after', { nullable: true }) after: string
+  ) {
+    const [hasNextPage, totalNumber, res] = await this.nftService.getNftByWalletAddress(
+      walletAddress.toLowerCase(),
+      take,
+      after
+    )
+    let endCursor = ''
+    if (res.length > 0) {
+      // this.console.log();
+      console.log(res[res.length - 1].create_date)
+      endCursor = Buffer.from(res[res.length - 1].create_date.toISOString()).toString('base64')
+    }
+    return {
+      endCursor: endCursor,
+      hasNextPage: hasNextPage,
+      nodes: res,
+      totalCount: totalNumber
+    }
   }
 
   @ResolveField(() => [NftBalanceEntity], { nullable: true })
