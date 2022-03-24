c97422100da4c96549ea808db7beb85920c79844
larryro <371767072@qq.com>
2022-03-24 18:32:08 +0800

    change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,13 +7,13 @@ module.exports = {
       exec_mode: "cluster",
       autorestart: true,
       watch: false,
-      max_memory_restart: '2G',
+      max_memory_restart: '4G',
       increment_var : 'PORT',
       instance_var: 'INSTANCE_ID',
       env_test: {
         NODE_ENV: 'test',
         PORT: 2999,
-        max_memory_restart: '2G',
+        max_memory_restart: '4G',
         // TZ: 'Asia/Shanghai'
       },
       env_production: {
