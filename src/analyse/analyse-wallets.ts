ef7417d58f48fa532d1f253f4670e040f92eb96f
larryro <371767072@qq.com>
2022-04-12 09:20:16 +0800

    modify

---
+++ src/analyse/analyse-wallets.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from 'src/app.module'
+import { WalletModule } from 'src/block-chain/wallet/wallet.module'
+import { WalletsAnalyseService } from 'src/block-chain/wallet/wallets-analyse.service'
+
+async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(AppModule)
+  const service = app.select(WalletModule).get(WalletsAnalyseService, { strict: true })
+  while (1) {
+    console.log('do while')
+    await service.analyseData()
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    // await sleep(1000)
+  }
+}
+bootstrap()
---
+++ src/block-chain/wallet/wallets.service.ts
@@ -0,0 +1,271 @@
+import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common'
+import { thetaTsSdk } from 'theta-ts-sdk'
+import { Cache } from 'cache-manager'
+import { MarketService } from '../../market/market.service'
+import BigNumber from 'bignumber.js'
+import { BalanceModel, StakeBalanceType, TotalBalanceType } from './wallet-balance.model'
+import { fetch } from 'cross-fetch'
+import { InjectRepository } from '@nestjs/typeorm'
+import { MoreThan, Repository } from 'typeorm'
+import { WalletEntity } from './wallet.entity'
+import { ActiveWalletsEntity } from './active-wallets.entity'
+const config = require('config')
+const moment = require('moment')
+@Injectable()
+export class WalletService {
+  logger = new Logger()
+
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+
+    @InjectRepository(WalletEntity, 'wallet')
+    private walletRepository: Repository<WalletEntity>,
+
+    @InjectRepository(ActiveWalletsEntity, 'wallet')
+    private activeWalletsRepository: Repository<ActiveWalletsEntity>,
+
+    private marketInfo: MarketService
+  ) {
+    thetaTsSdk.blockchain.setUrl(config.get('THETA_NODE_HOST'))
+  }
+
+  public async getBalanceByAddress(address: string) {
+    const accountBalance = await thetaTsSdk.blockchain.getAccount(address)
+    const thetaBalance = {
+      amount: Number(
+        new BigNumber(accountBalance.result.coins.thetawei).dividedBy('1e18').toFixed()
+      ),
+      fiat_currency_value: {
+        usd:
+          (await this.marketInfo.getThetaMarketInfo()).price *
+          Number(new BigNumber(accountBalance.result.coins.thetawei).dividedBy('1e18').toFixed()),
+        cny: 0,
+        eur: 0
+      }
+    }
+    const usdRate = await this.getUsdRate()
+    thetaBalance.fiat_currency_value.cny = thetaBalance.fiat_currency_value.usd * usdRate.CNY
+    thetaBalance.fiat_currency_value.eur = thetaBalance.fiat_currency_value.usd * usdRate.EUR
+    const thetaFuelBalance = {
+      amount: Number(
+        new BigNumber(accountBalance.result.coins.tfuelwei).dividedBy('1e18').toFixed()
+      ),
+      fiat_currency_value: {
+        usd:
+          (await this.marketInfo.getThetaFuelMarketInfo()).price *
+          Number(new BigNumber(accountBalance.result.coins.tfuelwei).dividedBy('1e18').toFixed()),
+        cny: 0,
+        eur: 0
+      }
+    }
+    thetaFuelBalance.fiat_currency_value.cny =
+      thetaFuelBalance.fiat_currency_value.usd * usdRate.CNY
+    thetaFuelBalance.fiat_currency_value.eur =
+      thetaFuelBalance.fiat_currency_value.usd * usdRate.EUR
+    return {
+      theta: thetaBalance,
+      theta_fuel: thetaFuelBalance
+    }
+  }
+
+  async getStakeInfoByAddress(address: string) {
+    const latestBlockHeight = (await thetaTsSdk.blockchain.getStatus()).result
+      .latest_finalized_block_height
+    const gcpStake: Array<StakeBalanceType> = []
+    const eenpStake: Array<StakeBalanceType> = []
+    const vcpStake: Array<StakeBalanceType> = []
+    const gcpList = await thetaTsSdk.blockchain.getGcpByHeight(latestBlockHeight)
+    const thetaMarketInfo = await this.marketInfo.getThetaMarketInfo()
+    const thetaFuelMarketInfo = await this.marketInfo.getThetaFuelMarketInfo()
+    const usdRate = await this.getUsdRate()
+
+    gcpList.result.BlockHashGcpPairs[0].Gcp.SortedGuardians.forEach((guardian) => {
+      guardian.Stakes.forEach((stake) => {
+        if (stake.source === address) {
+          gcpStake.push({
+            node_address: guardian.Holder,
+            amount: Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()),
+            withdrawn: stake.withdrawn,
+            return_height: stake.return_height,
+            fiat_currency_value: {
+              usd:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaMarketInfo.price,
+              cny:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaMarketInfo.price *
+                usdRate.CNY,
+              eur:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaMarketInfo.price *
+                usdRate.EUR
+            }
+          })
+        }
+      })
+    })
+
+    const eenpList = await thetaTsSdk.blockchain.getEenpByHeight(latestBlockHeight)
+
+    eenpList.result.BlockHashEenpPairs[0].EENs.forEach((een) => {
+      een.Stakes.forEach((stake) => {
+        if (stake.source === address) {
+          eenpStake.push({
+            node_address: een.Holder,
+            amount: Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()),
+            withdrawn: stake.withdrawn,
+            return_height: stake.return_height,
+            fiat_currency_value: {
+              usd:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaFuelMarketInfo.price,
+              cny:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaFuelMarketInfo.price *
+                usdRate.CNY,
+              eur:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaFuelMarketInfo.price *
+                usdRate.EUR
+            }
+          })
+        }
+      })
+    })
+
+    const validatorList = await thetaTsSdk.blockchain.getVcpByHeight(latestBlockHeight)
+
+    validatorList.result.BlockHashVcpPairs[0].Vcp.SortedCandidates.forEach((vcp) => {
+      vcp.Stakes.forEach((stake) => {
+        if (stake.source === address) {
+          vcpStake.push({
+            node_address: vcp.Holder,
+            amount: Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()),
+            withdrawn: stake.withdrawn,
+            return_height: stake.return_height,
+            fiat_currency_value: {
+              usd:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaMarketInfo.price,
+              cny:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaMarketInfo.price *
+                usdRate.CNY,
+              eur:
+                Number(new BigNumber(stake.amount).dividedBy('1e18').toFixed()) *
+                thetaMarketInfo.price *
+                usdRate.EUR
+            }
+          })
+        }
+      })
+    })
+
+    return {
+      stake_to_guardian: gcpStake,
+      stake_to_elite_node: eenpStake,
+      stake_to_vcp: vcpStake
+    }
+  }
+
+  public async getALlBalance(address: string): Promise<BalanceModel> {
+    const totalBalance: TotalBalanceType = {
+      fiat_currency_value: {
+        usd: 0,
+        cny: 0,
+        eur: 0
+      },
+      theta_amount: 0,
+      theta_fuel_amount: 0
+    }
+    let walletBalance = await this.getBalanceByAddress(address)
+    totalBalance.theta_amount += walletBalance.theta.amount
+    totalBalance.theta_fuel_amount += walletBalance.theta_fuel.amount
+    totalBalance.fiat_currency_value.usd +=
+      walletBalance.theta_fuel.fiat_currency_value.usd + walletBalance.theta.fiat_currency_value.usd
+    totalBalance.fiat_currency_value.cny +=
+      walletBalance.theta_fuel.fiat_currency_value.cny + walletBalance.theta.fiat_currency_value.cny
+    totalBalance.fiat_currency_value.eur +=
+      walletBalance.theta_fuel.fiat_currency_value.eur + walletBalance.theta.fiat_currency_value.eur
+    let stakeBalance = await this.getStakeInfoByAddress(address)
+    stakeBalance.stake_to_guardian.concat(stakeBalance.stake_to_vcp).forEach((stakeInfo) => {
+      totalBalance.theta_amount += stakeInfo.amount
+      totalBalance.fiat_currency_value.usd += stakeInfo.fiat_currency_value.usd
+      totalBalance.fiat_currency_value.cny += stakeInfo.fiat_currency_value.cny
+      totalBalance.fiat_currency_value.eur += stakeInfo.fiat_currency_value.eur
+    })
+    stakeBalance.stake_to_elite_node.forEach((stakeInfo) => {
+      totalBalance.theta_fuel_amount += stakeInfo.amount
+      totalBalance.fiat_currency_value.usd += stakeInfo.fiat_currency_value.usd
+      totalBalance.fiat_currency_value.cny += stakeInfo.fiat_currency_value.cny
+      totalBalance.fiat_currency_value.eur += stakeInfo.fiat_currency_value.eur
+    })
+    return {
+      total: totalBalance,
+      theta: walletBalance.theta,
+      theta_fuel: walletBalance.theta_fuel,
+      stake_to_guardian: stakeBalance.stake_to_guardian,
+      stake_to_elite_node: stakeBalance.stake_to_elite_node,
+      stake_to_validator_node: stakeBalance.stake_to_vcp
+    }
+  }
+
+  public async getUsdRate(): Promise<{ CNY: number; EUR: number }> {
+    const key = 'usd-rate-key'
+    if (await this.cacheManager.get(key)) return await this.cacheManager.get(key)
+    const res = await fetch('https://api.exchangerate-api.com/v4/latest/USD', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    if (res.status >= 400) {
+      throw new Error('Bad response from server')
+    }
+    // console.log(await res.json())
+    let jsonInfo = await res.json()
+    // console.log(res.json())
+    await this.cacheManager.set(key, jsonInfo['rates'], { ttl: 60 * 60 * 24 * 7 })
+    return jsonInfo['rates']
+  }
+
+  public async markActive(wallets: Array<{ address: string; timestamp: number }>): Promise<void> {
+    try {
+      const sqlArr = []
+      for (const wallet of wallets) {
+        // sqlArr.push(`('${wallet.address}', ${wallet.timestamp})`)
+        sqlArr.push({
+          address: wallet.address,
+          latest_active_time: wallet.timestamp
+        })
+        // this.logger.debug(sqlArr.join(','))
+        if (sqlArr.length > 900) {
+          await this.walletRepository.upsert(sqlArr, ['address'])
+
+          sqlArr.length = 0
+        }
+      }
+      await this.walletRepository.upsert(sqlArr, ['address'])
+    } catch (e) {
+      this.logger.error('update wallet fail')
+      this.logger.error(e)
+    }
+
+    // console.log(record)
+    // if (record) {
+    //   record.latest_active_time = timestamp
+    //   await this.walletRepository.save(record)
+    // } else {
+    //   let walletEntity = new WalletEntity()
+    //   walletEntity.address = address
+    //   walletEntity.latest_active_time = timestamp
+    //   await this.walletRepository.save(walletEntity)
+    // }
+  }
+
+  public async getActiveWallet(startTime) {
+    return await this.activeWalletsRepository.find({
+      snapshot_time: MoreThan(startTime)
+    })
+  }
+}
