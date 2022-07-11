ea43afad0d59921c4be921479ed603b6931b7c10
larryro <371767072@qq.com>
2022-07-11 09:59:44 +0800

    fix bug

--- src/block-chain/explorer/explorer-analyse.service.ts
+++ src/block-chain/explorer/explorer-analyse.service.ts
@@ -132,7 +132,6 @@ export class ExplorerAnalyseService {
             from = transaction.raw.from.address
             to = transaction.raw.to.address
           }
-
           break
         case THETA_TRANSACTION_TYPE_ENUM.smart_contract:
           from = transaction.raw.from.address
@@ -155,9 +154,13 @@ export class ExplorerAnalyseService {
           break
         // to =
         default:
-          from = transaction.raw.from
-            ? transaction.raw.from.address
-            : transaction.raw.source.address
+          if (transaction.raw.from) from = transaction.raw.from.address
+          else if (transaction.raw.source) {
+            from = transaction.raw.source.address
+          } else {
+            //@ts-ignore
+            from = transaction.holder.address
+          }
           break
       }
       await this.explorerConnection.manager.insert(TransactionEntity, {
