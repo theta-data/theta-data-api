7f8fa355d99909826586d4276bf7adac1757c01e
larryro <371767072@qq.com>
2022-07-12 10:27:37 +0800

    get count from count entity

--- src/block-chain/explorer/explorer.service.ts
+++ src/block-chain/explorer/explorer.service.ts
@@ -1,3 +1,5 @@
+import { BLOCK_COUNT_KEY, TRANSACTION_COUNT_KEY } from './const'
+import { CountEntity } from './count.entity'
 import { TransactionEntity } from './transaction.entity'
 import { BlokcListEntity } from './block-list.entity'
 import { Injectable, Logger } from '@nestjs/common'
@@ -12,7 +14,10 @@ export class ExplorerService {
     private blockListRepository: Repository<BlokcListEntity>,
 
     @InjectRepository(TransactionEntity, 'explorer')
-    private transactionRepository: Repository<TransactionEntity>
+    private transactionRepository: Repository<TransactionEntity>,
+
+    @InjectRepository(CountEntity, 'explorer')
+    private countRepository: Repository<CountEntity>
   ) {}
 
   public async getBlockList(
@@ -33,7 +38,11 @@ export class ExplorerService {
       this.logger.debug('decode from base64:' + height)
       condition.where[height] = LessThan(height)
     }
-    const totalBlock = await this.blockListRepository.count()
+    const totalBlock = (
+      await this.countRepository.findOne({
+        key: BLOCK_COUNT_KEY
+      })
+    ).count
     let blockList = await this.blockListRepository.find(condition)
     let hasNextPage = false
     if (blockList.length > take) {
@@ -61,7 +70,11 @@ export class ExplorerService {
       this.logger.debug('decode from base64:' + id)
       condition.where['id'] = LessThan(id)
     }
-    const totalBlock = await this.transactionRepository.count()
+    const totalBlock = (
+      await this.countRepository.findOne({
+        key: TRANSACTION_COUNT_KEY
+      })
+    ).count
     let blockList = await this.transactionRepository.find(condition)
     let hasNextPage = false
     if (blockList.length > take) {
