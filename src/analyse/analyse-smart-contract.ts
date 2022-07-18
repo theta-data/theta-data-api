49d95af79b2246a7403e80976455f294f0f9abea
larryro <371767072@qq.com>
2022-07-18 14:38:10 +0800

    change analyse logic

--- src/analyse/analyse-smart-contract.ts
+++ src/analyse/analyse-smart-contract.ts
@@ -5,13 +5,17 @@ import { SmartContractModule } from 'src/block-chain/smart-contract/smart-contra
 const config = require('config')
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule)
-  const service = app.select(SmartContractModule).get(SmartContractAnalyseService, { strict: true })
   while (1) {
+    const app = await NestFactory.createApplicationContext(AppModule)
+    const service = app
+      .select(SmartContractModule)
+      .get(SmartContractAnalyseService, { strict: true })
+
     await service.analyseData()
     await new Promise((resolve) =>
       setTimeout(resolve, config.get('SMART_CONTRACT.ANALYSE_INTERVAL'))
     )
+    app.close()
   }
 }
 bootstrap()
--- src/analyse/analyse-wallets.ts
+++ src/analyse/analyse-wallets.ts
@@ -4,11 +4,12 @@ import { WalletModule } from 'src/block-chain/wallet/wallet.module'
 import { WalletsAnalyseService } from 'src/block-chain/wallet/wallets-analyse.service'
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule)
-  const service = app.select(WalletModule).get(WalletsAnalyseService, { strict: true })
   while (1) {
+    const app = await NestFactory.createApplicationContext(AppModule)
+    const service = app.select(WalletModule).get(WalletsAnalyseService, { strict: true })
     await service.analyseData()
     await new Promise((resolve) => setTimeout(resolve, 1000))
+    app.close()
     // await sleep(1000)
   }
 }
