989082d3408764200e7d44f99b563402343f8200
larryro <371767072@qq.com>
2022-07-18 11:44:25 +0800

    adjust analyse logic

--- src/analyse/analyse-explorer.ts
+++ src/analyse/analyse-explorer.ts
@@ -7,11 +7,12 @@ import { NftModule } from 'src/block-chain/smart-contract/nft/nft.module'
 const config = require('config')
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule)
-  const service = app.select(ExplorerModule).get(ExplorerAnalyseService, { strict: true })
   while (1) {
+    const app = await NestFactory.createApplicationContext(AppModule)
+    const service = app.select(ExplorerModule).get(ExplorerAnalyseService, { strict: true })
     await service.analyseData()
     await new Promise((resolve) => setTimeout(resolve, config.get('EXPLORER.ANALYSE_INTERVAL')))
+    app.close()
   }
 }
 bootstrap()
--- src/app.module.ts
+++ src/app.module.ts
@@ -40,10 +40,10 @@ const config = require('config')
       ...config.get('ORM_CONFIG'),
       database: config.get('ORM_CONFIG')['database'] + 'smart_contract/smart_contract.sqlite',
       name: 'smart_contract',
-      prepareDatabase: (obj) => {
-        obj.pragma('journal_size_limit', { journal_size_limit: 1024 * 1024 * 100 })
-        console.log('prepare database obj', obj)
-      },
+      // prepareDatabase: (obj) => {
+      //   obj.pragma('journal_size_limit', { journal_size_limit: 1024 * 1024 * 100 })
+      //   console.log('prepare database obj', obj)
+      // },
       entities: []
     }),
     TypeOrmModule.forRoot({
@@ -86,10 +86,10 @@ const config = require('config')
       ...config.get('ORM_CONFIG'),
       database: config.get('ORM_CONFIG')['database'] + 'explorer/data.sqlite',
       name: 'explorer',
-      prepareDatabase: (obj) => {
-        obj.pragma('journal_size_limit', { journal_size_limit: 1024 * 1024 * 100 })
-        console.log('prepare database obj', obj)
-      },
+      // prepareDatabase: (obj) => {
+      //   obj.pragma('journal_size_limit', { journal_size_limit: 1024 * 1024 * 100 })
+      //   console.log('prepare database obj', obj)
+      // },
 
       entities: []
     }),
